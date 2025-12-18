7aeb976d0d4afc0ecc10a26566ae5f4b033d3a6b
soggycoding <soggycodes@gmail.com>
2025-12-19 05:03:19 +0800

    Updated PyGuided#4.py
    learned how to apply class and use def for cleaner code, will now try to modify it more to make it more appealing and comprehensable

--- PyGuided#4.py
+++ PyGuided#4.py
@@ -1,23 +1,27 @@
 ## First Project, a grocery list that will ask for your input for the things you need and will list them down after you finish writing it.
 ## it will ask what you'll input and will have a confirmation (y/n) to avoid mistakes or errors
 ## if possible, it should also ask for the last time if there will be any revisions to the list before giving the final print
-product_list = []
-while True:
-    question = input("Would you like to add something in the list? (yes/no) ").lower()
-   
-    if question == "yes":
-        product_name = input("Input the product you want to add to the list: ")
-        confirmation = input(f"Are you sure that you want to add " + product_name.upper() + " to the list? (y/n)").lower()
-        if confirmation == "y":
-            product_list.append(product_name)
-            print("Succesfully added to the list.")
-        elif confirmation == "n":
-            product_name = input("Input the correct product you want to add: ")
-            product_list.append(product_name)
-            
-    elif question == "no":
+added = []
+class add_cart:
+    def __init__(self):
+        self.product_name = input("Input the product you want to add to the list: ").upper()
         
-        print(f"Here is the grocery list\n", product_list)
+
+while True:
+    question = input("Would you like to add something in the list? (y/n) ").lower()
+   
+    if question == "y":
+        product = add_cart()
+        confirmation = input("Are you sure that you want to add " + product.product_name + " to the list? (y/n)")
+        if confirmation == "yes":
+            added.insert(product.product_name)
+            print("Succesfully added", product.product_name, "to the list.")
+        elif confirmation == "no":
+            product_name = input("Input the correct product you want to add: ")
+            
+    elif question == "n":
+        
+        print("Here is the grocery list\n", added)
         break
     
     else:
