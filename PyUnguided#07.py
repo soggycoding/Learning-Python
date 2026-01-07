f92748c2c400ca3533a375282354d72037427534
Soggy Codes <soggycodes@gmail.com>
2026-01-07 18:54:51 +0800

    Learned how to do wait using time.sleep will implement it in my other project which is the #6 since it will be useful

--- PyUnguided#07.py
+++ PyUnguided#07.py
@@ -1,3 +1,10 @@
+#New project, I will try to make a lyrics typing output where I will understand the time module and delays
+import sys
+import time
+
 with open('Documents/wiwiws.txt', mode='r', encoding="utf-8") as file:
     for line in file:
-        print(line.strip())
+        print(line.strip(), end = '\n')
+        sys.stdout.flush()
+        time.sleep(1)
+        
