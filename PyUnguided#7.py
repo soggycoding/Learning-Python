2a2332b149ac0da297a19fd2a9d87e9e084e15fd
Soggy Codes <soggycodes@gmail.com>
2026-01-07 18:02:20 +0800

    This is a test to change my vsc directory

---
+++ PyUnguided#7.py
@@ -0,0 +1,11 @@
+#Trying File I/O
+import os
+print(os.getcwd())
+#Writing a File
+"""
+with open('test.txt', mode="w", encoding="utf-8") as file:
+    file.write("Bahog bilat\n bingbong\n bingbing\n balabigbog")
+with open('wiwiws.txt', mode="r", encoding="utf-8") as file:
+    for line in file:
+        print(line.strip())
+"""
