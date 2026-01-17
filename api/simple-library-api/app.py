50ca1ece9152968007c35e6f84eaee269c7b4afa
Soggy Codes <soggycodes@gmail.com>
2026-01-17 18:17:00 +0800

    DAY1: Planning for my project which is a Library API

---
+++ api/simple-library-api/app.py
@@ -0,0 +1,14 @@
+#3rd project will be a Library API
+'''
+1. It should have a title, author, and description about the book
+2. It should not allow the user to input a book without an author nor description
+3. It should also have a unique ID generator since it will be a library
+4. It should also allow the user to update the book
+5. It should also be filtered, it should be listed as alphabetical
+THINGS TO WORK ON:
+- variables should always be lowered case
+- URLS should also be lowered case
+- remove dummy tasks from initial data
+- CHECK FOR BUGS ALWAYS
+- Variable names
+'''
