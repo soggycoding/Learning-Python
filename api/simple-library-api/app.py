776f7f690313126b25ab54e45ce949180877dc3e
Soggy Codes <soggycodes@gmail.com>
2026-01-20 16:17:45 +0800

    DAY4: Have to redo everything because I did it wrong when I made claude evaulate. instead of feeling down, I want to correct it right away. Right now I am currently back to zero and doing authors rn and taking it slow.

--- api/simple-library-api/app.py
+++ api/simple-library-api/app.py
@@ -20,94 +20,46 @@ from flask import Flask, request
 app = Flask(__name__)
 books = []
 authors = []
-library = []
+library = [list(zip(authors, books))]
 
-#GETS THE WHOLE LIST OF BOOKS AND AUTHORS, ADDS A NEW LIST OF AUTHOR AND THEIR BOOK
-@app.route('/library', methods=['GET', 'POST'])
-def book_library():
-    if request.method == 'GET':
-        library = [list(zip(authors,books))]
-        return {"Library": library}, 200
-    
+
+@app.route('/authors', methods=['GET', 'POST'])
+def author():
     if request.method == 'POST':
         data = request.get_json()
-        author = data['Author']
+        name = data['Author']
         country = data['Country']
-        if author == "":
-            return {"error": "Contents not found"}, 404
+        if name == "":
+            return {"error": "Content not found"}, 400
         elif country == "":
-            return {"error": "Contents not found"}, 404
-        new_author = {'ID': id_author(),'Author': author, 'Country': country}
-        authors.append(new_author)
-        book = data['Title']
-        description = data['Description']
-        publication = data['Publication']
-        if book == "":
-            return {"error": "Contents not found"}, 404
-        elif description == "":
-            return {"error": "Contents not found"}, 404
-        elif publication == "":
-            return {"error": "Contents not found"}, 404
-        new_book = {'ID': id_book(), 'Title': book, 'Description': description, 'Publication': publication}
-        books.append(new_book)
-
-        return [new_author,new_book], 200
-#GET THE AUTHOR AND BOOK BY THEIR UNIQUE GENERATED ID, CAN UPDATE THEIR CONTENTS USING THEIR UNIQUE ID, CAN DELETE THEIR REQUESTED BOOK AND AUTHOR USING THEIR ID
-@app.route('/library/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-def sorted_book(id):
+            return {"error": "Content not found"}, 400
+        author = {'ID': id_author(), 'Author': name, 'Country': country}
+        authors.append(author)
+        return author, 200
+    if request.method == 'GET':
+        return {"Authors": authors}, 200
+    
+@app.route('/authors/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def author_id(id):
     if request.method == 'GET':
         for author in authors:
             if author['ID'] == id:
-                for book in books:
-                    if books['ID'] == id:
-                        return [author, book]
-        return {"error": "Contents not found"}, 404
-    
-    if request.method == 'PUT':
-        for author in authors:
-            if author['ID'] == id:
-                authors.remove(author)
-                data = request.get_json()
-                author['Author'] = data['Author']
-                author['Country'] = data['Country']
-                if author == "":
-                    return {"error": "Contents not found"}, 404
-                authors.append(author)
-                for book in books:
-                    if book['ID'] == id:
-                        books.remove(book)
-                        data = request.get_json()
-                        book['Title'] = data['Title']
-                        book['Description'] = data['Description']
-                        book['Publication'] = data['Publication']
-                        if book == "":
-                            return {"error": "Contents not found"}, 404
-                        books.append(book)
-                        return [author, book]
-        return {"error": "Contents not found"}, 404
-                
-    if request.method == 'DELETE':
-        for author in authors:
-            if author['ID'] == id:
-                authors.remove(author)
-                for book in books:
-                    if book['ID'] == id:
-                        books.remove(book)
-                        return "Success", 200
-        return {"error": "Contents not found"}, 404
-                
-def id_book():
+                return author, 200
+        return{"error": "Content not found"}, 404
+'''           
+def id():
     if not books:
         return 1
     else:
         ids = [book['ID'] for book in books]
         return max(ids)+1
-    
+'''
 def id_author():
     if not authors:
         return 1
     else:
         ids = [author['ID'] for author in authors]
         return max(ids)+1
+
 if __name__ == '__main__':
     app.run(debug=True)
