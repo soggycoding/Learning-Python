8cb39193cc1844cc80a8795cd642a3ade97368d4
Soggy Codes <soggycodes@gmail.com>
2026-01-22 17:32:45 +0800

    DAY1: Doing my 4th API hardcoded project. An expense tracker and also trying to make a habit out of doing README's within the same file of my project so I'll know what to do whenever I take a rest

---
+++ api/expense_tracker/README.md
@@ -0,0 +1,46 @@
+Markdowns to remember:
+'#' For headings
+'-' for bullet points
+'code' for inline code
+'''code block''' for code blocks
+'[x]' for completed checkboxes
+'[]' for incomplete checkboxes
+
+# Expense Tracker API
+
+My 4th project for hardcoded API to prepare for learning DB. It is an  expense tracker that has categories for different types of expenses. You can get the total for everything and you can also get the total for a specific category.
+
+## Things to consider:
+
+- Should be filtered by category
+- it should sum all the expenses
+- it should also sum all of the expenses for specific category
+
+## Project overview:
+
+With this project, it should help me with:
+- Fundamentals of CRUD, and API
+- Build a solid foundation for API's and how it works
+- Build confidence in coding and give me a better understanding for planning
+
+## ENDPOINTS:
+
+### CATEGORIES:
+- `POST /category`Creates a new category
+- `GET /category` Retrieves all the category
+- `UPDATE /category/<int:id>` Update a specific category
+- `GET /category/<int:id>` Retrieves a specific category
+- `DELETE /category/<int:id>`Deletes a specific category
+
+### EXPENSES:
+- `POST /expenses` Create a new expense
+- `GET /expenses` Retrieves all the expenses
+- `UPDATE /expenses/<int:id>` Update a specific expense
+- `GET /expenses/<int:id>` Retrieve a specific expense
+- `DELETE /expenses/<int:id>` Delete a specific expense
+
+## PROJECT STATUS:
+
+- [x] Phase 1: Categories in CRUD
+- [] Phase 2: Expenses CRUD with validation
+- [] Phase 3: Enhanced responses with full category details
---
+++ api/expense_tracker/app.py
@@ -0,0 +1,47 @@
+#EXPENSE TRACKER
+from flask import Flask, request
+
+app = Flask(__name__)
+categories = []
+@app.route('/category', methods=['POST', 'GET'])
+def expense_category():
+    if request.method == 'POST':
+        data = request.get_json()
+        expense_category = data['']
+        if expense_category == '':
+            return {"error": "Missing required fields"}, 400
+        category = {'id': category_id(), 'category': expense_category}
+        categories.append(category)
+        return category, 201
+    
+    if request.method == 'GET':
+        return categories, 200
+@app.route('/category/<int:id>', methods=['PUT', 'GET', 'DELETE'])
+def category(id):
+    if request.method == 'PUT':
+        for category in categories:
+            if category['id'] == id:
+                data = request.get_json()
+                category['category'] = data['']
+                return category, 200
+        return {"error": "Content not found"}, 404
+    if request.method == 'GET':
+        for category in categories:
+            if category['id'] == id:
+                return category, 200
+        return {"error": "Content not found"}, 404
+    if request.method == 'DELETE':
+        for category in categories:
+            if category['id'] == id:
+                categories.remove(category)
+                return{"message": "Successfully deleted"}, 200
+        return {"error": "Content not found"}, 404
+            
+def category_id():
+    if not categories:
+        return 1
+    else:
+        ids = [category['id'] for category in categories]
+        return max(ids)+1
+if __name__ == '__main__':
+    app.run(debug=True)
