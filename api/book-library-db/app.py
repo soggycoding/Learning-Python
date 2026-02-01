53626b4f51bee5a649d6b0fdc137d256837341d4
Soggy Codes <soggycodes@gmail.com>
2026-02-01 16:14:48 +0800

    Finished converting 2nd DB project

--- api/book-library-db/app.py
+++ api/book-library-db/app.py
@@ -11,17 +11,17 @@ db = SQLAlchemy(app)
 class Authors(db.Model):
     __tablename__ = 'authors'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    author_name = db.Column(db.String(50), unique=True, nullable=False)
     country = db.Column(db.String(120), nullable=False)
     books = db.relationship('Books', backref='author', lazy=True)
     
     def __repr__(self):
-        return f'<Added: {self.name}'
+        return f'<Added: {self.author_name}'
     
     def to_dict(self):
         return {
             'id': self.id,
-            'name': self.name,
+            'name': self.author_name,
             'country': self.country
         }
     
@@ -46,7 +46,7 @@ class Books(db.Model):
         if include_author:
             data['author'] =  {
                 'id': self.author.id,
-                'name': self.author.name,
+                'author_name': self.author.author_name,
                 'country': self.author.country
             }
             
@@ -58,17 +58,20 @@ if __name__ == '__main__':
 
 with app.app_context():
     db.drop_all()
-    db.create_all()
-'''       
+    db.create_all()    
+''' 
 
 @app.route('/books', methods=['POST', 'GET'])
 def add_books():
     if request.method == 'POST':
         data = request.get_json()
         if 'title' not in data or 'description' not in data or 'author_id' not in data:
-            return {"error": "Missing required fields"}, 404
+            return {"error": "Missing required fields"}, 400
         if not data['title'] or not data['description'] or not data['author_id']:
-            return {"error": "Missing required fields"}, 404
+            return {"error": "Missing required fields"}, 400
+        author = Authors.query.get(data['author_id'])
+        if not author:
+            return {"error": "Author not found"}, 404
         new_book = Books(title=data['title'], description=data['description'], author_id=data['author_id'])
         db.session.add(new_book)
         db.session.commit()
@@ -84,27 +87,31 @@ def book_id(id):
     if request.method == 'PUT':
         books = Books.query.filter_by(id=id).first()
         if not books:
-            return {"error": "Content not found"}, 400
+            return {"error": "Content not found"}, 404
         data = request.get_json()
         if 'title' not in data or 'description' not in data:
-            return {"error": "Missing required fields"}, 404
+            return {"error": "Missing required fields"}, 400
         if not data['title'] or not data['description']:
-            return {"error": "Missing required fields"}, 404
+            return {"error": "Missing required fields"}, 400
+        author = Authors.query.get(data['author_id'])
+        if not author:
+            return {"error": "Author not found"}, 404
         books.title = data['title']
         books.description = data['description']
+        books.author_id = data['author_id']
         db.session.commit()
         return books.to_dict(include_author=False), 200
     
     if request.method == 'GET':
         books = Books.query.filter_by(id=id).first()
         if not books:
-            return {"error": "Content not found"}, 400
+            return {"error": "Content not found"}, 404
         return books.to_dict(), 200
     
     if request.method == 'DELETE':
         books = Books.query.filter_by(id=id).first()
         if not books:
-            return {"error": "Content not found"}, 400
+            return {"error": "Content not found"}, 404
         db.session.delete(books)
         db.session.commit()
         return {"message": "Successfully removed"}, 200
@@ -114,10 +121,10 @@ def add_authors():
     if request.method == 'POST':
         data = request.get_json()
         if 'author' not in data or 'country' not in data:
-            return {"error": "Missing required fields"}, 404
+            return {"error": "Missing required fields"}, 400
         if not data['author'] or not data['country']:
-            return {"error": "Missing required fields"}, 404
-        new_author = Authors(name=data['author'], country=data['country'])
+            return {"error": "Missing required fields"}, 400
+        new_author = Authors(author_name=data['author'], country=data['country'])
         db.session.add(new_author)
         db.session.commit()
         return new_author.to_dict(), 201
@@ -132,13 +139,13 @@ def author_id(id):
     if request.method == 'PUT':
         authors = Authors.query.filter_by(id=id).first()
         if not authors:
-            return {"error": "Content not found"}, 400
+            return {"error": "Content not found"}, 404
         data = request.get_json()
-        if not data['author'] or not data ['country']:
-            return {"error": "Missing required fields"}, 404
         if 'author' not in data or 'country' not in data:
-            return {"error": "Missing required fields"}, 404
-        authors.name = data['author']
+            return {"error": "Missing required fields"}, 400
+        if not data['author'] or not data ['country']:
+            return {"error": "Missing required fields"}, 400
+        authors.author_name = data['author']
         authors.country = data['country']
         db.session.commit()
         return authors.to_dict(), 200
@@ -146,13 +153,13 @@ def author_id(id):
     if request.method == 'GET':
         authors = Authors.query.filter_by(id=id).first()
         if not authors:
-            return {"error": "Content not found"}, 400
+            return {"error": "Content not found"}, 404
         return authors.to_dict(), 200
     
     if request.method == 'DELETE':
         authors = Authors.query.filter_by(id=id).first()
         if not authors:
-            return{"error": "Content not found"}, 400
+            return{"error": "Content not found"}, 404
         db.session.delete(authors)
         db.session.commit()
         return {"message": "Successfully removed"}, 200
