bc7cdf3377b6e34392fb2d5de5301cac45bc5176
Soggy Codes <soggycodes@gmail.com>
2026-02-20 17:13:58 +0800

    Added a README

---
+++ api/movie-tags/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+
