186d223310fe08d958b92f7f1eec80960ce96448
Soggy Codes <soggycodes@gmail.com>
2026-02-21 17:55:46 +0800

    Doing many-to-many and one-to-many database

---
+++ api/movie-tags/README.md
@@ -0,0 +1,58 @@
+Markdowns to remember: 
+'#' For headings 
+'-' for bullet points 
+'code' for inline code 
+'''code block''' for code blocks 
+'[x]' for completed checkboxes 
+'[]' for incomplete checkboxes
+
+# MOVIE TAGS DATABASE(MANY-TO-MANY, ONE-TO-MANY)
+- RESTFUL API Combines reviews, tags and rating suggestions
+
+### PRE-REQUISITES
+- Python
+- Flask
+- SQLAlchemy
+
+### INSTALLATION
+
+## TECHSTACK
+- Python
+- Flask
+- SQLAlchemy
+
+## DATANASE SCHEMA
+
+## API ENDPOINTS
+
+### REVIEWS
+`POST /reviews` - Adds a new review
+`GET /reviews` - Retrieves all the reviews
+`PUT /reviews/<int:id>` - Updates a specific review
+`GET /reviews/<int:id>` - Retrieves a specific review
+`DELETE /reviews/int:id>` - Deletes a specific review
+
+### TAGS
+`POST /tags` - Adds a new tag
+`GET /tags` - Retrieves all the tags
+`PUT /tags/<int:id>` - Updates a specific tag
+`GET /tags/<int:id>` - Retrieve a specific tag
+`DELETE /tags/<int:id>` - Deletes a specific tag
+
+### MOVIES
+`POST /movies` - Adds a new movie
+`GET /movies` - Retrives all the movies
+`PUT /movies/<int:id>` - Updates a specific movie
+`GET /movies/<int:id>` - Retrieves a specific movie
+`DELETE /movies/<int:id>` - Deletes a specific movie
+
+## PROJECT OVERVIEW AND FEATURES
+- Connects one-to-many relationship and many-to-many relationship
+
+## PROJECT STATUS
+[] PHASE 1: CRUD for Reviews and Tags
+[] PHASE 2: Validator
+[] PHASE 3: One-to-Many relationship
+[] PHASE 4: Many-to-Many relationship
+[] PHASE 5: 
+[] PHASE 6:
--- api/movie-tags/app.py
+++ api/movie-tags/app.py
@@ -8,3 +8,64 @@ app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
 
+# Many-to-Many(Association Object)
+class TagsReviews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review_id = db.Column('review_id', db.Integer, db.ForeignKey('review.id'))
+    tag_id = db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+    
+    review = db.relationship('Reviews', back_populates='tagsreviews')
+    tag = db.relationship('Tags', back_populates='tagsreviews')
+
+
+class Reviews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String(80), unique=True, nullable=False)
+    tagsreviews = db.relationship('TagsReviews', back_populates='review',
+                                  cascade='all, delete-orphan')
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "review": self.review
+        }
+class Tags(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(30), unique=True,nullable=False)
+    tagsreviews = db.relationship('TagsReviews', back_populates='tag',
+                                  cascade='all, delete-orphan')
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "tag" : self.tag
+        }
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+@app.route('/reviews', methods=['POST', 'GET'])
+def add_review():
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'review' not in data:
+            return {"error": "Missing required fields"}, 400
+        if not data['review']:
+            return {"error": "Missing required fields"}, 400
+        reviews = Reviews(review=data['review'])
+        db.session.add(reviews)
+        db.session.commit()
+        return reviews.to_dict(), 201
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
