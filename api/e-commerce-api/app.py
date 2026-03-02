4506c73c1b04edecac6d4e9454a6b0cb48e4e737
Soggy Codes <soggycodes@gmail.com>
2026-03-02 17:17:41 +0800

    Did a little bit, but I really am not feeling like it today. Will continue tomorrow

--- api/e-commerce-api/app.py
+++ api/e-commerce-api/app.py
@@ -48,26 +48,62 @@ class Orders(db.Model):
             "status" : self.status,
             "created_date" : self.created_date
         }
-        
+'''       
 class OrderItems(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column('order_id', db.Integer, db.ForeignKey('orders.id'))
-    product_id = db.Column('product_id', db.Integer, db.Foreign('products.id'))
+    product_id = db.Column('product_id', db.Integer, db.ForeignKey('products.id'))
     quantity = db.Column(db.Integer, nullable=False)
     price_at_purchase = db.Columm(db.Integer, nullable=False)
     
     def to_dict(self):
         return {
             "id" : self.id,
-            "order_id" : self.order_id,
+            "order_id" : self.order_id,z
             "product_id" : self.product_id,
             "quantity" : self.quantity,
             "price_at_purchase" : self.price_at_purchase
         }
-    
+'''
+'''
 with app.app_context():
     db.drop_all()
     db.create_all()
+'''
+@app.route('/products', methods=['POST', 'GET'])
+def add_products():
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'name' not in data or 'description' not in data or 'price' not in data or 'stock' not in data:
+            return {"error" : "Missing required fields"}, 400
+        if not data['name'] or not data['description'] or not data['price'] or not data['stock']:
+            return {"error" : "Missing required fields"}, 400
+        product = Products(name=data['name'], description=data['description'], price=data['price'], stock=data['stock'])
+        db.session.add(product)
+        db.session.commit()
+        return product.to_dict(), 201
     
+    if request.method == 'GET':
+        products = Products.query.all()
+        product_list = [product.to_dict() for product in products]
+        return {"products" : product_list}, 200
+
+@app.route('/products/<int:id>', methods=['PUT', 'GET', 'DELETE'])
+def update_products(id):
+    if request.method == 'PUT':
+        product = Products.query.filter_by(id=id).first()
+        if not product:
+            return {"error" : "Product not found"}, 404
+        data = request.get_json()
+        if 'name' not in data or 'description' not in data or 'price' not in data or 'stock' not in data:
+            return {"error" : "Missing required fields"}, 400
+        if not data['name'] or not data['description'] or not data['price'] or not data['stock']:
+            return {"error" : "Missing required fields"}, 400
+        product.name = data['name']
+        product.description = data['description']
+        product.price = data['price'],
+        product.stock = data['stock']
+        db.session.commit()
+        return product.to_dict(), 200
 if __name__ == '__main__':
     app.run(debug=True)
