d9e47cc029f1ace96b775ac40cdd2c20b414f6bf
Soggy Codes <soggycodes@gmail.com>
2026-04-06 15:05:07 +0800

    Finished CRUD for Company, Interview, and JobApplication. Will continue tomorrow

--- api/job-application-tracker-api/app.py
+++ api/job-application-tracker-api/app.py
@@ -14,7 +14,7 @@ class Company(db.Model):
     name = db.Column(db.String(40), nullable=False)
     industry = db.Column(db.String(40), nullable=False)
     
-    #jobapplication = db.relationship("JobApplication", backref='jobapplication', lazy=True)
+    jobapplication = db.relationship("JobApplication", backref='jobapplication', lazy=True)
     
     def to_dict(self):
         return {
@@ -29,8 +29,8 @@ class JobApplication(db.Model):
     applied_date = db.Column(db.DateTime, default=datetime.now)
     notes = db.Column(db.String(60), nullable=True)
     
-    #company = db.relationship('Company', backref='company', lazy=True)
-    #interview = db.relationship('Interview', backref='jobapplication', lazy=True)
+    company = db.relationship('Company', backref='company', lazy=True)
+    interview = db.relationship('Interview', backref='jobapplication', lazy=True)
     
     def to_dict(self):
         return {
@@ -46,21 +46,22 @@ class Interview(db.Model):
     scheduled_date = db.Column(db.String(40), nullable=False)
     outcome = db.Column(db.String(20), nullable=False)
     
-    #jobapplication = db.relationship('JobApplication', backref='interview', lazy=True)
+    jobapplication = db.relationship('JobApplication', backref='interview', lazy=True)
     
     
     def to_dict(self):
         return {
             "round" : self.round,
             "scheduled_date" : self.scheduled_date,
-            "outscome" : self.outcome
+            "outcome" : self.outcome
         }
-    '''    
-    with app.app_context():
-        db.drop_all()
-        db.create_all()  
-    '''
-@app.route ('/company', methods=['POST', 'GET'])
+'''    
+with app.app_context():
+    db.drop_all()
+    db.create_all()  
+'''
+   
+@app.route ('/companies', methods=['POST', 'GET'])
 def add_company():
     if request.method == "POST":
         data = request.get_json()
@@ -78,9 +79,9 @@ def add_company():
         company_list = [company.to_dict() for company in companies]
         return {
             "companies" : company_list
-        }
+        }, 200
 
-@app.route ('/company/<int:id>', methods=['PUT', 'GET', 'DELETE'])
+@app.route ('/companies/<int:id>', methods=['PUT', 'GET', 'DELETE'])
 def update_company(id):
     if request.method == "PUT":
         company = Company.query.filter_by(id=id).first()
@@ -111,5 +112,111 @@ def update_company(id):
         db.session.delete(company)
         db.session.commit()
         return {"message": "Company deleted successfully"}, 200
+    
+@app.route ('/interviews', methods=['POST', 'GET'])
+def add_interview():
+    if request.method == "POST":
+        data = request.get_json()
+        if 'round' not in data or 'scheduled_date' not in data or 'outcome' not in data:
+            return {"error" : "Missing required fields"}, 400
+        if not data['round'] or not data['scheduled_date'] or not data['outcome']:
+            return {"error" : "Missing required fields"}, 400
+        interview = Interview(round=data['round'], scheduled_date=data['scheduled_date'], outcome=data['outcome'])
+        db.session.add(interview)
+        db.session.commit()
+        return interview.to_dict(), 201
+
+    if request.method == "GET":
+        interviews = Interview.query.all()
+        interview_list = [interview.to_dict() for interview in interviews]
+        return {
+            "Interviews" : interview_list
+            }, 200
+    
+@app.route('/interviews/<int:id>', methods=['PUT', 'GET', 'DELETE'])
+def update_interview(id):
+    if request.method == 'PUT':
+        interview = Interview.query.filter_by(id=id).first()
+        if not interview:
+            return {"error" : "Interview not found"}, 404
+        data = request.get_json()
+        if 'round' not in data or 'scheduled_date' not in data or 'outcome' not in data:
+            return {"error" : "Missing required fields"}, 400
+        if not data['round'] or not data['scheduled_date'] or not data['outcome']:
+            return {"error" : "Missing required fields"}, 400
+        interview.round = data['round']
+        interview.scheduled_date = data['scheduled_date']
+        interview.outcome = data['outcome']
+        db.session.commit()
+        return interview.to_dict()
+    
+    if request.method == 'GET':
+        interview = Interview.query.filter_by(id=id).first()
+        if not interview:
+            return {"error" : "Interview not found"}, 404
+        return {
+            "Interview" : interview.to_dict()
+            }, 200
+        
+    if request.method == 'DELETE':
+        interview = Interview.query.filter_by(id=id).first()
+        if not interview:
+            return {"error" : "Interview not found"}, 404
+        db.session.delete(interview)
+        db.session.commit()
+        return {"message" : "Interview deleted successfully"}, 200
+    
+@app.route('/jobapplications', methods=['POST', 'GET'])
+def add_jobapplication():
+    if request.method == 'POST':
+        data = request.get_json()
+        if "role" not in data or "status" not in data:
+            return {"error" : "Missing required fields"}, 400
+        if not data['role'] or not data['status']:
+            return {"error" : "Missing required fields"}, 400
+        jobapplication = JobApplication(role=data['role'], status=data['status'], notes=data['notes'])
+        db.session.add(jobapplication)
+        db.session.commit()
+        return jobapplication.to_dict()
+    
+    if request.method == 'GET':
+        jobapplications = JobApplication.query.all()
+        jobapplication_list = [jobapplication.to_dict() for jobapplication in jobapplications]
+        return {"Job Application" : jobapplication_list}, 200
+
+@app.route('/jobapplications/<int:id>', methods=['PUT', 'GET', 'DELETE'])
+def update_jobapplication(id):
+    if request.method == 'PUT':
+        jobapplication = JobApplication.query.filter_by(id=id).first()
+        if not jobapplication:
+            return {"error" : "Job Application not found"}, 404
+        data = request.get_json()
+        if "role" not in data or "status" not in data:
+            return {"error" : "Missing required fields"}, 400
+        if not data['role'] or not data['status']:
+            return {"error" : "Missing required fields"}, 400
+        jobapplication.role = data['role']
+        jobapplication.status = data['status']
+        jobapplication.notes = data['notes']
+        db.session.commit()
+        return jobapplication.to_dict(), 200
+    
+    if request.method == 'GET':
+        jobapplication = JobApplication.query.filter_by(id=id).first()
+        if not jobapplication:
+            return {"error" : "Job Application not found"}, 404
+        return {
+            "Job Application" : jobapplication.to_dict()
+            }, 200
+    
+    if request.method == 'DELETE':
+        jobapplication = JobApplication.query.filter_by(id=id).first()
+        if not jobapplication:
+            return {"error" : "Job Application not found"}, 404
+        db.session.delete(jobapplication)
+        db.session.commit()
+        return {"message" : "Job Application deleted successfully"}
+
+@app.route('/jobapplications/<int:jobapplication_id>')
 if __name__ == '__main__':
     app.run(debug=True)
