f7f231f16f7904869ee5bca7b52404cf4ee4d269
Soggy Codes <soggycodes@gmail.com>
2026-08-20 19:01:14 +0800

    Did some exercises but still not done with day 11, i also started late by 30minutes

--- 30days_python/day_11/exercise_day_11.py
+++ 30days_python/day_11/exercise_day_11.py
@@ -1,4 +1,5 @@
 # Functions
+'''
 def add_two_numbers(num1, num2):
     sum = num1 + num2
     return sum
@@ -9,11 +10,30 @@ def area_of_circle(r):
     area = PI * r ** r
     return area
 print(area_of_circle(10))
-
-def add_all_nums(num):
-    while num is int:
-        num + num
-        if num != int:
-            print("A value is not an integer ")
-            pass
-print(add_all_nums(23))
+'''
+'''
+        def add_all_nums(*num):
+            total = 0
+            for nums in num:
+                if nums != int:
+                    print("Arguement is not equal to int")
+                total += nums 
+            return total
+        print(add_all_nums(23,24,26))
+'''
+'''
+def convert_celcius_to_fahrenheit(celc):
+    fahrenheit = (celc * 9 / 5) + 32
+    return fahrenheit
+print(convert_celcius_to_fahrenheit(24))
+'''
+def check_season(month):
+    if month == ["March","April","May"]:
+        return "The season is Spring"
+    if month == ["June","July","August"]:
+        return "The season is Summer"
+    if month == ["September","November","October"]:
+        return "The season is Autumn"
+    if month == ["December","January"," February"]:
+        return "The season is Winter"
+check_season("December")
