eb6275c0330735cc90e1112e8bcedb09219f38a5
Soggy Codes <soggycodes@gmail.com>
2026-08-21 19:01:15 +0800

    Still doing exercises for day 11 and I am now enjoying it even more

--- 30days_python/day_11/exercise_day_11.py
+++ 30days_python/day_11/exercise_day_11.py
@@ -27,13 +27,39 @@ def convert_celcius_to_fahrenheit(celc):
     return fahrenheit
 print(convert_celcius_to_fahrenheit(24))
 '''
+'''
 def check_season(month):
-    if month == ["March","April","May"]:
-        return "The season is Spring"
-    if month == ["June","July","August"]:
-        return "The season is Summer"
-    if month == ["September","November","October"]:
-        return "The season is Autumn"
-    if month == ["December","January"," February"]:
-        return "The season is Winter"
-check_season("December")
+    spring = ['March', 'April', 'May']
+    summer =['June', 'July', 'August'] 
+    autumn = ['September', 'October', 'November']
+    winter =  ['December', 'January', 'February']
+    if month in spring:
+        return print("The season is Spring")
+    if month in summer:
+        return print("The season is Summer")
+    if month in autumn:
+        return print("The season is Autumn")
+    if month in winter:
+        return print("The season is Winter")
+
+check_season("February")
+'''
+'''
+def calculate_slope(x1, x2, y1, y2):
+    slope = (y2 - y1) / (x2 - x1)
+    return print(slope)
+calculate_slope(2, 5, 10, 1)
+'''
+'''
+        def solve_quadratic_eqn(a,b,c):
+            quadratic = a*1**2 + b*1 + c
+            return print(quadratic)
+        solve_quadratic_eqn(-2,2,1)
+'''
+'''
+def print_list(*items):
+    for elements in items:
+        print(elements)
+print_list('potato', 'banana', 'hotdog', 'bente', 'wiowiwi')
+'''
+def reverse_list(*items):
